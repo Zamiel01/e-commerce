56bcbe2494ca4ea1ed440a6fec250ea03d7d6dde
Zamiel01 <nkuinmbeng@gmail.com>
2025-01-29 09:47:21 +0100

    done

--- ecommerce/users/urls.py
+++ ecommerce/users/urls.py
@@ -1,12 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+from .views import UserViewSet
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, UserRegistrationView, UserLoginView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('login/', UserLoginView.as_view(), name='user-login'),
-]
+urlpatterns = router.urls
